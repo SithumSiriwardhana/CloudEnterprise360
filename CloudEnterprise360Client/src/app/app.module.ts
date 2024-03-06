64c8774d91a5fd8e73472e2c4f79615c64507e7d
Sithum Siriwardhana <sithumdharshana22@gmail.com>
2024-03-07 03:11:35 +0530

    Initail Client Structure

--- CloudEnterprise360Client/src/app/app.module.ts
+++ CloudEnterprise360Client/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
